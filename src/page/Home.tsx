142b796326cff7d6a0b8dbcd988193a9d754bf8c
MAHossain1 <mahpro110@gmail.com>
2023-07-15 05:24:33 +0600

    books shows on home page using redux query done

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import BookCard from "../components/BookCard";
+import { useGetBooksQuery } from "../redux/api/apiSlice";
+import { IBook } from "../types/globalTypes";
+
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { data, isLoading, error } = useGetBooksQuery(undefined);
+
+  console.log(data);
+
   return (
-    <div>
-      <h1>hello home basi</h1>
+    <div className="grid mt-5 grid-cols-12 max-w-7xl mx-auto relative ">
+      <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
+        {data?.data?.map((book: IBook) => (
+          <BookCard book={book} key={book._id} />
+        ))}
+      </div>
+      <div className="col-span-3 z mr-10 space-y-5 border rounded-2xl border-gray-200/80 p-5 self-start sticky top-16 h-[calc(100vh-80px)]">
+        search bar
+      </div>
     </div>
   );
 }
