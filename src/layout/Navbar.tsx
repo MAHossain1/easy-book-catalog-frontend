f038772bd265f97aadb640634b2d440f40d7afa1
MAHossain1 <mahpro110@gmail.com>
2023-07-15 03:25:38 +0600

    redux setup and get books query done

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -7,8 +7,11 @@ export default function Navbar() {
     <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
       <div className="h-full w-full bg-gray-200">
         <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
-          <div>
+          <div className="flex items-center">
             <img className="h-8" src={logo} alt="log" />
+            <h1 className="text-bold text-2xl ml-2 text-accent">
+              Easy Book Catalog
+            </h1>
           </div>
           <div>
             <ul className="flex items-center">
