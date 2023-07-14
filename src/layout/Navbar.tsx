aad25d8ba83c8999d352b42d7b22de36cd06ec35
MAHossain1 <mahpro110@gmail.com>
2023-07-14 16:19:57 +0600

    navbar done

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,75 +1,38 @@
+import { Link } from "react-router-dom";
+
+import logo from "../assets/images/book-catalog.png";
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
+    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
+      <div className="h-full w-full bg-gray-200">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <div>
+            <img className="h-8" src={logo} alt="log" />
+          </div>
+          <div>
+            <ul className="flex items-center">
+              <button className="btn btn-ghost">
                 <li>
-                  <a>Submenu 1</a>
+                  <Link to="/login">Login</Link>
                 </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+              </button>
+              <li>
+                <button className="btn btn-ghost">
+                  <Link to="/signup">Sign up</Link>
+                </button>
+              </li>
+              <li>
+                <button className="btn btn-ghost">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
-      </div>
-    </div>
+    </nav>
   );
 }
