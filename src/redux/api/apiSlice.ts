0a0d2c2c2c63ce5aa686096f1bb2088246b5874a
MAHossain1 <mahpro110@gmail.com>
2023-07-16 20:06:18 +0600

    backend live link uploaded done

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -5,7 +5,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://easy-book-catalog-backend.vercel.app/books",
+  }),
   tagTypes: ["comments"],
   endpoints: builder => ({
     getBooks: builder.query({
