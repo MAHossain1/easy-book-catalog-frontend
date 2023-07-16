6cbbcdde64c52a5eedbffb50e5888152099f2e02
MAHossain1 <mahpro110@gmail.com>
2023-07-16 19:49:34 +0600

    apiSlice update done

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -6,10 +6,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
-
+  tagTypes: ["comments"],
   endpoints: builder => ({
     getBooks: builder.query({
       query: () => "/books",
+      providesTags: ["comments"],
     }),
     updateBook: builder.mutation({
       query: ({ id, book }) => ({
@@ -17,6 +18,7 @@ export const api = createApi({
         method: "PATCH",
         body: book,
       }),
+      invalidatesTags: ["comments"],
     }),
     singleBook: builder.query({
       query: id => `/book/${id}`,
@@ -27,6 +29,7 @@ export const api = createApi({
         method: "POST",
         body: data,
       }),
+      invalidatesTags: ["comments"],
     }),
     postBook: builder.mutation({
       query: book => ({
